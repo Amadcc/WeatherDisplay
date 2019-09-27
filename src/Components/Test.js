6b57f8ccd9422bf5763ddbb1987bbc7fd066cb42
Amad S <amadc2207@gmail.com>
2019-09-27 19:20:31 +0600

    style deleted

--- src/Components/Test.js
+++ src/Components/Test.js
@@ -1,27 +1,13 @@
 import React, { Component } from "react";
 import GetWeather from "./Components/GetWeather"
-import styled from "styled-components";
 
 class App extends Component {
 
   render() {
     return (
-      <Wrapper>
         <GetWeather />
-      </Wrapper>
     );
   }
 }
 
 export default App;
-
-const Wrapper = styled.div`
-  text-align: center;
-  nav ul li {
-    display: inline-block;
-    margin: 0.5em;
-    a.active {
-      font-weight: bold;
-    }
-  }
-`;
